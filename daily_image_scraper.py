95d3cf37155fcf030c5ef07429755937117c5fcd
meisty <shaundixon666@gmail.com>
2017-06-26 22:01:04 +0100

    First commit of new repo

---
+++ daily_image_scraper.py
@@ -0,0 +1,34 @@
+import requests, bs4, os
+
+def getDailyImage(imageUrl):
+    url=imageUrl
+    res = requests.get(imageUrl)
+    res.raise_for_status
+
+    soup = bs4.BeautifulSoup(res.text, 'html.parser')
+    imageElem = soup.select('html body center p a img')
+    if imageElem == []:
+        print("Could not find the image")
+    else:
+        imgUrl = imageElem[0].get('src')
+        imgUrl = url+imgUrl
+        #download the image
+        print("Downloading the image %s..." % (imgUrl))
+        res = requests.get(imgUrl)
+        res.raise_for_status
+        print(res)
+
+    with open('background.jpg', 'wb') as fo:
+        for chunk in res.iter_content(4096):
+            fo.write(chunk)
+
+
+    os.system("/usr/bin/gsettings set org.gnome.desktop.background picture-uri file:/home/shaun/python/webscraping/background.jpg")
+
+
+
+
+
+
+getDailyImage('https://apod.nasa.gov/apod/')
+print("Your image has been downloaded and has been set as your desktop background")
